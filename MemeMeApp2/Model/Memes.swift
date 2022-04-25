6fcb8cbf00fe450063f26b878b224a0a9893b9fc
joel stevick <joelstevick@outlook.com>
2022-04-25 14:41:34 -0400

    add memes singleton

---
+++ MemeMeApp2/Model/Memes.swift
@@ -0,0 +1,48 @@
+//
+//  Memes.swift
+//  MemeMeApp2
+//
+//  Created by Joel Stevick on 4/25/22.
+//
+
+import Foundation
+
+private var globalId = 0
+
+struct MemeListEntry {
+    
+    let id: Int
+    let meme: Meme
+    
+    init(_ meme: Meme) {
+        globalId += 1
+        
+        id = globalId
+        self.meme = meme
+    }
+}
+struct Memes {
+    
+    static let shared = Memes()
+    
+    private var memes: [MemeListEntry] = []
+    private static var singleton: Memes?
+    
+    mutating func add(_ meme: Meme) {
+        memes.append(MemeListEntry( meme))
+    }
+    
+    func getMemes() -> [MemeListEntry] {
+        return memes
+    }
+    
+    mutating func removeMeme(entry: MemeListEntry) {
+        memes = memes.filter({ e in
+            e.id != entry.id
+        })
+    }
+    
+    private init() {
+        
+    }
+}
